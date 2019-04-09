312a5be1e89451036866ca2bafcb522e49de8e3e
Aurélien David <spyl94@gmail.com>
2019-04-09 10:02:57 +0200

    Remove report-badge.js

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,40 +1,36 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _regenerator = require('babel-runtime/regenerator');
+var _regenerator = require("babel-runtime/regenerator");
 
 var _regenerator2 = _interopRequireDefault(_regenerator);
 
-var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+var _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator");
 
 var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
-var _path = require('path');
+var _path = require("path");
 
 var _path2 = _interopRequireDefault(_path);
 
-var _config = require('./cli/config');
+var _config = require("./cli/config");
 
-var _flow = require('./flow');
+var _flow = require("./flow");
 
-var _promisified = require('./promisified');
+var _promisified = require("./promisified");
 
-var _reportHtml = require('./report-html');
+var _reportHtml = require("./report-html");
 
 var _reportHtml2 = _interopRequireDefault(_reportHtml);
 
-var _reportBadge = require('./report-badge');
-
-var _reportBadge2 = _interopRequireDefault(_reportBadge);
-
-var _reportJson = require('./report-json');
+var _reportJson = require("./report-json");
 
 var _reportJson2 = _interopRequireDefault(_reportJson);
 
-var _reportText = require('./report-text');
+var _reportText = require("./report-text");
 
 var _reportText2 = _interopRequireDefault(_reportText);
 
@@ -63,24 +59,24 @@ exports.default = function () {
             projectDir = opts.projectDir;
             tmpDirPath = void 0;
 
-            if (!(process.env.VERBOSE && process.env.VERBOSE === 'DUMP_JSON')) {
+            if (!(process.env.VERBOSE && process.env.VERBOSE === "DUMP_JSON")) {
               _context.next = 7;
               break;
             }
 
             _context.next = 5;
-            return (0, _promisified.withTmpDir)('flow-coverage-report');
+            return (0, _promisified.withTmpDir)("flow-coverage-report");
 
           case 5:
             tmpDirPath = _context.sent;
 
-            console.log('Verbose DUMP_JSON mode enabled (' + tmpDirPath + ')');
+            console.log("Verbose DUMP_JSON mode enabled (" + tmpDirPath + ")");
 
           case 7:
 
-            opts.flowCommandPath = opts.flowCommandPath || 'flow';
+            opts.flowCommandPath = opts.flowCommandPath || "flow";
             opts.flowCommandTimeout = opts.flowCommandTimeout || _config.DEFAULT_FLOW_TIMEOUT; // Defaults to 15s
-            opts.outputDir = opts.outputDir || './flow-coverage';
+            opts.outputDir = opts.outputDir || "./flow-coverage";
             opts.outputDir = _path2.default.isAbsolute(opts.outputDir) ? opts.outputDir : _path2.default.resolve(_path2.default.join(projectDir, opts.outputDir));
             opts.globIncludePatterns = opts.globIncludePatterns || [];
             opts.globExcludePatterns = opts.globExcludePatterns || [];
@@ -97,7 +93,7 @@ exports.default = function () {
               break;
             }
 
-            return _context.abrupt('return', Promise.reject(new TypeError('projectDir option is mandatory')));
+            return _context.abrupt("return", Promise.reject(new TypeError("projectDir option is mandatory")));
 
           case 17:
             if (!(opts.globIncludePatterns.length === 0)) {
@@ -105,7 +101,7 @@ exports.default = function () {
               break;
             }
 
-            return _context.abrupt('return', Promise.reject(new TypeError('empty globIncludePatterns option')));
+            return _context.abrupt("return", Promise.reject(new TypeError("empty globIncludePatterns option")));
 
           case 19:
             if (opts.threshold) {
@@ -113,7 +109,7 @@ exports.default = function () {
               break;
             }
 
-            return _context.abrupt('return', Promise.reject(new TypeError('threshold option is mandatory')));
+            return _context.abrupt("return", Promise.reject(new TypeError("threshold option is mandatory")));
 
           case 21:
             _context.next = 23;
@@ -122,34 +118,34 @@ exports.default = function () {
           case 23:
             coverageData = _context.sent;
             reportResults = [];
-            reportTypes = opts.reportTypes || ['text'];
+            reportTypes = opts.reportTypes || ["text"];
 
 
-            if (reportTypes.indexOf('json') >= 0) {
+            if (reportTypes.indexOf("json") >= 0) {
               reportResults.push(_reportJson2.default.generate(coverageData, opts));
             }
 
-            if (reportTypes.indexOf('text') >= 0) {
+            if (reportTypes.indexOf("text") >= 0) {
               reportResults.push(_reportText2.default.generate(coverageData, opts));
             }
 
             // Run the badge reporter implicitly if the html report has been included.
-            if (reportTypes.indexOf('badge') >= 0 || reportTypes.indexOf('html') >= 0) {
-              reportResults.push(_reportBadge2.default.generate(coverageData, opts));
+            if (reportTypes.indexOf("badge") >= 0 || reportTypes.indexOf("html") >= 0) {
+              // reportResults.push(reportBadge.generate(coverageData, opts));
             }
 
-            if (reportTypes.indexOf('html') >= 0) {
+            if (reportTypes.indexOf("html") >= 0) {
               reportResults.push(_reportHtml2.default.generate(coverageData, opts).then(function () {
-                console.log('View generated HTML Report at file://' + opts.outputDir + '/index.html');
+                console.log("View generated HTML Report at file://" + opts.outputDir + "/index.html");
               }));
             }
 
-            return _context.abrupt('return', Promise.all(reportResults).then(function () {
+            return _context.abrupt("return", Promise.all(reportResults).then(function () {
               return [coverageData, opts];
             }));
 
           case 31:
-          case 'end':
+          case "end":
             return _context.stop();
         }
       }
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,21 +1,20 @@
-'use strict';
+"use strict";
 
 // @flow
 
-import path from 'path';
+import path from "path";
 
-import {DEFAULT_FLOW_TIMEOUT} from './cli/config';
-import {collectFlowCoverage} from './flow';
-import {withTmpDir} from './promisified';
-import reportHTML from './report-html';
-import reportBadge from './report-badge';
-import reportJSON from './report-json';
-import reportText from './report-text';
+import { DEFAULT_FLOW_TIMEOUT } from "./cli/config";
+import { collectFlowCoverage } from "./flow";
+import { withTmpDir } from "./promisified";
+import reportHTML from "./report-html";
+import reportJSON from "./report-json";
+import reportText from "./report-text";
 
 // eslint-disable-next-line no-duplicate-imports
-import type {ConfigParams, ReportType} from './cli/config';
+import type { ConfigParams, ReportType } from "./cli/config";
 // eslint-disable-next-line no-duplicate-imports
-import type {FlowCoverageSummaryData} from './flow';
+import type { FlowCoverageSummaryData } from "./flow";
 
 export type FlowCoverageReportType = ReportType;
 
@@ -33,22 +32,25 @@ export type FlowCoverageReportOptions = {
 // 6. set a custom output dir
 // 7. usa a saved json file to compute coverage trend (and fail on negative trends)
 
-export default async function generateFlowCoverageReport(opts: FlowCoverageReportOptions) {
+export default async function generateFlowCoverageReport(
+  opts: FlowCoverageReportOptions
+) {
   // Apply defaults to options.
   const projectDir = opts.projectDir;
 
   let tmpDirPath: ?string;
 
-  if (process.env.VERBOSE && process.env.VERBOSE === 'DUMP_JSON') {
-    tmpDirPath = await withTmpDir('flow-coverage-report');
+  if (process.env.VERBOSE && process.env.VERBOSE === "DUMP_JSON") {
+    tmpDirPath = await withTmpDir("flow-coverage-report");
     console.log(`Verbose DUMP_JSON mode enabled (${tmpDirPath})`);
   }
 
-  opts.flowCommandPath = opts.flowCommandPath || 'flow';
+  opts.flowCommandPath = opts.flowCommandPath || "flow";
   opts.flowCommandTimeout = opts.flowCommandTimeout || DEFAULT_FLOW_TIMEOUT; // Defaults to 15s
-  opts.outputDir = opts.outputDir || './flow-coverage';
-  opts.outputDir = path.isAbsolute(opts.outputDir) ?
-    opts.outputDir : path.resolve(path.join(projectDir, opts.outputDir));
+  opts.outputDir = opts.outputDir || "./flow-coverage";
+  opts.outputDir = path.isAbsolute(opts.outputDir)
+    ? opts.outputDir
+    : path.resolve(path.join(projectDir, opts.outputDir));
   opts.globIncludePatterns = opts.globIncludePatterns || [];
   opts.globExcludePatterns = opts.globExcludePatterns || [];
   opts.concurrentFiles = opts.concurrentFiles || 1;
@@ -59,40 +61,46 @@ export default async function generateFlowCoverageReport(opts: FlowCoverageRepor
 
   // Apply validation checks.
   if (!projectDir) {
-    return Promise.reject(new TypeError('projectDir option is mandatory'));
+    return Promise.reject(new TypeError("projectDir option is mandatory"));
   }
 
   if (opts.globIncludePatterns.length === 0) {
-    return Promise.reject(new TypeError('empty globIncludePatterns option'));
+    return Promise.reject(new TypeError("empty globIncludePatterns option"));
   }
 
   if (!opts.threshold) {
-    return Promise.reject(new TypeError('threshold option is mandatory'));
+    return Promise.reject(new TypeError("threshold option is mandatory"));
   }
 
   const coverageData: FlowCoverageSummaryData = await collectFlowCoverage(
-    opts, tmpDirPath);
+    opts,
+    tmpDirPath
+  );
 
   const reportResults = [];
-  const reportTypes = opts.reportTypes || ['text'];
+  const reportTypes = opts.reportTypes || ["text"];
 
-  if (reportTypes.indexOf('json') >= 0) {
+  if (reportTypes.indexOf("json") >= 0) {
     reportResults.push(reportJSON.generate(coverageData, opts));
   }
 
-  if (reportTypes.indexOf('text') >= 0) {
+  if (reportTypes.indexOf("text") >= 0) {
     reportResults.push(reportText.generate(coverageData, opts));
   }
 
   // Run the badge reporter implicitly if the html report has been included.
-  if (reportTypes.indexOf('badge') >= 0 || reportTypes.indexOf('html') >= 0) {
-    reportResults.push(reportBadge.generate(coverageData, opts));
+  if (reportTypes.indexOf("badge") >= 0 || reportTypes.indexOf("html") >= 0) {
+    // reportResults.push(reportBadge.generate(coverageData, opts));
   }
 
-  if (reportTypes.indexOf('html') >= 0) {
-    reportResults.push(reportHTML.generate(coverageData, opts).then(() => {
-      console.log(`View generated HTML Report at file://${opts.outputDir}/index.html`);
-    }));
+  if (reportTypes.indexOf("html") >= 0) {
+    reportResults.push(
+      reportHTML.generate(coverageData, opts).then(() => {
+        console.log(
+          `View generated HTML Report at file://${opts.outputDir}/index.html`
+        );
+      })
+    );
   }
 
   return Promise.all(reportResults).then(() => {
